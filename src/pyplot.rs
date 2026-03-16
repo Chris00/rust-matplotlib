3f9643628920453f56a368a6352cdafe34aa82e9
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2026-03-16 15:26:36 +0100

    Remove the `pyplot` helper function

--- src/pyplot.rs
+++ src/pyplot.rs
@@ -22,26 +22,14 @@ use crate::{axes::Axes, Error, figure::Figure, IntoError};
 //
 // => Do not use pyplot interface (since we need handles anyway).
 
-fn pyplot<'a, 'py>(
-    py: Python<'py>,
-    f: &'a PyOnceLock<Py<PyAny>>,
-    attr_name: &str
-) -> Result<&'a Bound<'py, PyAny>, Error> {
-    f.get_or_try_init(py, || {
-        Ok(py.import("matplotlib.pyplot").into_error(py)?
-            .getattr(attr_name).into_error(py)?
-            .unbind())
-    })
-        .map(|f| f.bind(py))
-}
-
 /// Return a new figure.
 /// This figure is tracked by Matplotlib so [`show()`] displays it.
 /// This implies it must be explicitly deallocated using [`close()`].
 pub fn figure() -> Result<Figure, Error> {
     static FIG: PyOnceLock<Py<PyAny>> = PyOnceLock::new();
     Python::attach(|py| {
-        let fig = pyplot(py, &FIG, "figure")?;
+        let fig = FIG.import(py, "matplotlib.pyplot", "figure")
+            .into_error(py)?;
         Ok(Figure { fig: fig.call0().into_error(py)?.unbind() })
     })
 }
@@ -58,7 +46,7 @@ pub fn subplots<const R: usize, const C: usize>(
 pub fn show() {
     static SHOW: PyOnceLock<Py<PyAny>> = PyOnceLock::new();
     Python::attach(|py| {
-        pyplot(py, &SHOW, "show")
+        SHOW.import(py, "matplotlib.pyplot", "show")
             .expect("Cannot get matplotlib.pyplot.show")
             .call0()
             .expect("matplotlib.pyplot.show did not succeed");
@@ -69,7 +57,7 @@ pub fn show() {
 pub fn close(fig: Figure) {
     static CLOSE: PyOnceLock<Py<PyAny>> = PyOnceLock::new();
     Python::attach(|py| {
-        pyplot(py, &CLOSE, "close")
+        CLOSE.import(py, "matplotlib.pyplot", "close")
             .expect("Cannot find matplotlib.pyplot.close")
             .call1((fig.fig,))
             .expect("matplotlib.pyplot.close did not succeed");
@@ -80,7 +68,7 @@ pub fn close(fig: Figure) {
 pub fn close_all() {
     static CLOSE: PyOnceLock<Py<PyAny>> = PyOnceLock::new();
     Python::attach(|py| {
-        pyplot(py, &CLOSE, "close")
+        CLOSE.import(py, "matplotlib.pyplot", "close")
             .expect("Cannot get matplotlib.pyplot.close")
             .call1(("all",))
             .expect("matplotlib.pyplot.close('all') did not succeed");
